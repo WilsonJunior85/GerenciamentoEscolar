7a2b1a7afa9d51ad9f6e325f2fae52913d43af8e
WILSON JUNIOR <wilson.pereira@serede.com.br>
2025-07-21 16:15:47 -0300

    Conexão com o Banco

---
+++ GerenciamentoEscolar/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoEscolar.Data
+{
+    public class AppDbContext: DbContext
+    {
+
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+    }
+}
